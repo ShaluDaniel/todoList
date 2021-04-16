05ce5585d103bc3b4b109bd0cb118812c9b15001
Your Name <shalu.daniel98@gmail.com>
2021-04-16 22:48:17 +0530

    using AJAX

--- script.js
+++ script.js
@@ -40,25 +40,67 @@ function check(){
 
 
 
-$('#search').click(function() {
-	// var searchField = $('#search').val();
-	// var myExp = new RegExp(searchField, "i");
+// $('#search').click(function() {
+// 	// var searchField = $('#search').val();
+// 	// var myExp = new RegExp(searchField, "i");
 	
-	$.getJSON('data.json', function(data) {
-		var output="";
-		$.each(data, function(key, val) {
+// 	$.getJSON('data.json', function(data) {
+// 		var output="";
+// 		$.each(data, function(key, val) {
 			 
 			
-		 output +='<tr class="table-danger">'+'<td>'+'<input id="myCheck" type="checkbox">'+'</td>'+ '<td>'+val.title +'</td>'+'</tr>';	
+// 		 output +='<tr class="table-danger">'+'<td>'+'<input id="myCheck" type="checkbox">'+'</td>'+ '<td>'+val.title +'</td>'+'</tr>';	
 				
-		});
-		$('#rowtask').html(output);
+// 		});
+// 		$('#rowtask').html(output);
 		
 		
 		
 		
-	}); //get JSON
-});
+// 	}); //get JSON
+// });
 
+$('#search').click(function ajax(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        // condition
+        if(this.readyState==4&&this.status==200){
+            var response = JSON.parse(this.responseText);
+          
+            var output ="";
+            output += `<thead>
+                            <th>Status</th>
+                            <th>Task</th>
+                       </thead>`
+            for(var i=0;i<response.length;i++){
 
+                
+
+                if(response[i].completed == true){
+                    output += ` <tr>
+                        <td><input type="checkbox"  id="chkbx" checked disabled></td>
+                        <td style="text-decoration: line-through;">${response[i].title}</td>
+                    <tr>`;
+                }
+                else{
+                  output += 
+                                 `
+                                     <tr> 
+                                        <td><input type="checkbox" onclick="check();"></td>    
+                                        <td>${response[i].title}</td>
+                                     </tr>   
+                                   `;
+                }
+               
+                
+                      
+            }
+            document.getElementById("rowtask").innerHTML = output;
+        }
+    };
+    xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
+    xhttp.send();
+    })
+
+   
 				
